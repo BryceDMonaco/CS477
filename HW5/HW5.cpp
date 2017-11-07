3d157b1bc434ca8f0f322115b90a2b7c97c8cbe1
Bryce Monaco <brycedmonaco@gmail.com>
2017-11-06 19:37:10 -0800

    Completed HW5

--- HW5/HW5.cpp
+++ HW5/HW5.cpp
@@ -5,7 +5,7 @@
 
 	Compile with: g++ HW5.cpp -o HW5 -std=c++11
 
-	Note: Functions for each part have their respective part after their names (ex. SelectBestScheduleB is for part B)
+	Note: Functions for specific parts ONLY have their respective part after their names (ex. PrintChoicesB is for part B)
 */
 
 #include <iostream>
@@ -19,20 +19,40 @@ using namespace std;
 int lowJobs[] = {10, 1, 10, 10};
 int highJobs[] = {5, 50, 5, 1};
 
-vector<char> choicesB;
-char* choicesMade;
+int numberOfWeeks = 4;
 
-void PrintChoicesB (); //This is hardcoded to print four choices!!!
-int SelectBestScheduleB (int weekNumber, char previousChoice, int currentSum);
+//Global Vars for Part B
+char* choicesMade; 	//Choices are stored in order, even though they are evaluated in reverse order
+					//The Selection algorithm goes back and appends itself after it evaluates a split in the "decision tree" when a high job is chosen
+
+void PrintChoicesB ();
+void PrintChoicesC ();
+void PrintScheduleD (int totalRevenue);
+
+//Note: 	SelectBestSchedule is the algorithm for parts B, C, and D. It already stores the choices
+//			that it made in choicesMade. Part C and D
+int SelectBestSchedule (int weekNumber, char previousChoice, int currentSum, char* theseChoicesMade);
+void UpdateChoices (char* forkChoices, char* rootChoices, int forkWeekNumber);
 
 int main ()
 {
-	int numberOfWeeks = 4;
-	choicesMade = new char[numberOfWeeks];
+	choicesMade = new char[numberOfWeeks]; //Used to store choices for part b
+	int totalRevenue = SelectBestSchedule (numberOfWeeks - 1, 'X', 0, choicesMade);
 
-	cout << "Best total revenue is $" << SelectBestScheduleB (numberOfWeeks - 1, 'X', 0) << endl;
+	//Part B
+	cout << "Best total revenue is $" << totalRevenue << endl;
 
 	PrintChoicesB ();
+
+	//Part C
+	cout << endl;
+
+	PrintChoicesC ();
+
+	//Part D
+	cout << endl;
+
+	PrintScheduleD (totalRevenue);
 
 	return 0;
 
@@ -43,7 +63,7 @@ void PrintChoicesB ()
 	cout << "Part B output: " << endl << endl;
 
 	cout << "Week: ";
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < numberOfWeeks; i++)
 	{
 		cout << '\t' << i + 1;
 
@@ -51,7 +71,7 @@ void PrintChoicesB ()
 
 	cout << endl;
 
-	for (int i = 0; i <= 4; i++)
+	for (int i = 0; i <= numberOfWeeks; i++)
 	{
 		cout << "--------";
 
@@ -60,14 +80,81 @@ void PrintChoicesB ()
 	cout << endl;
 
 	cout << "High: ";
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < numberOfWeeks; i++)
+	{
+		if (choicesMade[i] == 'H')
+		{
+			cout << '\t' << "(" << highJobs[i] << ")";
+
+		} else
+		{
+			cout << '\t' << highJobs[i];
+
+		}
+
+	}
+
+	cout << endl << "Low: ";
+	for (int i = 0; i < numberOfWeeks; i++)
+	{
+		if (choicesMade[i] == 'L')
+		{
+			cout << '\t' << "(" << lowJobs[i] << ")";
+
+		} else
+		{
+			cout << '\t' << lowJobs[i];
+
+		}
+
+	}
+
+	cout << endl;
+
+	for (int i = 0; i <= numberOfWeeks; i++)
+	{
+		cout << "--------";
+
+	}
+
+	cout << endl << "Note:\tThe choice for a week is surrounded with '()'," << endl;
+	cout << "\tif a week has no choices then that means that \n\tweek is to be taken off." << endl;
+
+
+	return;
+
+}
+
+void PrintChoicesC ()
+{
+	cout << "Part C output: " << endl << endl;
+
+	cout << "Week: ";
+	for (int i = 0; i < numberOfWeeks; i++)
+	{
+		cout << '\t' << i + 1;
+
+	}
+
+	cout << endl;
+
+	for (int i = 0; i <= numberOfWeeks; i++)
+	{
+		cout << "--------";
+
+	}
+
+	cout << endl;
+
+	cout << "High: ";
+	for (int i = 0; i < numberOfWeeks; i++)
 	{
 		cout << '\t' << highJobs[i];
 
 	}
 
 	cout << endl << "Low: ";
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < numberOfWeeks; i++)
 	{
 		cout << '\t' << lowJobs[i];
 
@@ -75,14 +162,14 @@ void PrintChoicesB ()
 
 	cout << endl;
 
-	for (int i = 0; i <= 4; i++)
+	for (int i = 0; i <= numberOfWeeks; i++)
 	{
 		cout << "--------";
 
 	}
 
 	cout << endl << "Choice: ";
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < numberOfWeeks; i++)
 	{
 		cout << choicesMade[i] << '\t';
 
@@ -90,7 +177,7 @@ void PrintChoicesB ()
 
 	cout << endl;
 
-	for (int i = 0; i <= 4; i++)
+	for (int i = 0; i <= numberOfWeeks; i++)
 	{
 		cout << "--------";
 
@@ -103,8 +190,54 @@ void PrintChoicesB ()
 
 }
 
+void PrintScheduleD (int sentValue)
+{
+	cout << "Part D Output: " << endl << endl;
+
+	cout << "Best Schedule: (Generated Using Stored Choices):" << endl;
+
+	for (int i = 0; i < numberOfWeeks; i++)
+	{
+		char choice = choicesMade[i];
+
+		cout << "\tWeek " << i + 1 << ": ";
+
+		if (choice == 'L')
+		{
+			cout << "Low-Stress Job (Revenue: $" << lowJobs[i] << ")" << endl;
+
+		} else if (choice == 'H')
+		{
+			cout << "High-Stress Job (Revenue: $" << highJobs[i] << ")" << endl;
+
+		} else if (choice == 'X')
+		{
+			cout << "Take the week off" << endl;
+
+		}
+
+	}
+
+	cout << "Total Revenue: $" << sentValue << endl;
+
+	cout << endl << "Table of Stored Choices: ";
+
+	for (int i = 0; i < numberOfWeeks; i++)
+	{
+		cout << choicesMade[i] << '\t';
+
+	}
+
+	cout << endl << "Note:\tThe only extra information needed was the total revenue";
+	cout << "\n\twhich was computed recursively as the subproblems were solved.";
+	cout << "\n\tThese values are not important to reconstruct the solution so they are discarded." << endl;
+
+	return; 
+
+}
+
 //This works from week i all the way back to week 0
-int SelectBestScheduleB (int weekNumber, char previousChoice, int currentSum)
+int SelectBestSchedule (int weekNumber, char previousChoice, int currentSum, char* theseChoicesMade)
 {
 	if (weekNumber == -1)
 	{
@@ -114,30 +247,36 @@ int SelectBestScheduleB (int weekNumber, char previousChoice, int currentSum)
 	{
 		if (previousChoice == 'H')
 		{
-			choicesMade[weekNumber] = 'X';
-			return SelectBestScheduleB (weekNumber - 1, 'X', currentSum); //Must take a break for this week
+			theseChoicesMade[weekNumber] = 'X';
+			return SelectBestSchedule (weekNumber - 1, 'X', currentSum, theseChoicesMade); //Must take a break for this week
 
 		} else
 		{
 			if (lowJobs[weekNumber] >= highJobs[weekNumber])
 			{
-				choicesMade[weekNumber] = 'L';
-				return SelectBestScheduleB (weekNumber - 1, 'L', currentSum + lowJobs[weekNumber]); //The low stress job has higher revenue and is always part of the optimal solution
+				theseChoicesMade[weekNumber] = 'L';
+				return SelectBestSchedule (weekNumber - 1, 'L', currentSum + lowJobs[weekNumber], theseChoicesMade); //The low stress job has higher revenue and is always part of the optimal solution
 
 			} else if (highJobs[weekNumber] > lowJobs[weekNumber])
 			{
+				char* lowForkChoicesMade = new char[numberOfWeeks];
+				char* highForkChoicesMade = new char[numberOfWeeks];
 
-				int lowForkSum = SelectBestScheduleB (weekNumber - 1, 'L', currentSum + lowJobs[weekNumber]);
-				int highForkSum = SelectBestScheduleB (weekNumber - 1, 'H', currentSum + highJobs[weekNumber]);
+				int lowForkSum = SelectBestSchedule (weekNumber - 1, 'L', currentSum + lowJobs[weekNumber], lowForkChoicesMade);
+				int highForkSum = SelectBestSchedule (weekNumber - 1, 'H', currentSum + highJobs[weekNumber], highForkChoicesMade);
 
 				if (lowForkSum >= highForkSum)
 				{
-					choicesMade[weekNumber] = 'L';
+					UpdateChoices (lowForkChoicesMade, theseChoicesMade, weekNumber - 1);
+
+					theseChoicesMade[weekNumber] = 'L';
 					return lowForkSum;
 
 				} else
 				{
-					choicesMade[weekNumber] = 'H';
+					UpdateChoices (highForkChoicesMade, theseChoicesMade, weekNumber - 1);
+
+					theseChoicesMade[weekNumber] = 'H';
 					return highForkSum;
 
 				}
@@ -147,5 +286,17 @@ int SelectBestScheduleB (int weekNumber, char previousChoice, int currentSum)
 		}
 
 	}
+
+}
+
+void UpdateChoices (char* forkChoices, char* rootChoices, int forkWeekNumber)
+{
+	for (int i = 0; i <= forkWeekNumber; i++)
+	{
+		*(rootChoices + i) = *(forkChoices + i);
+
+	}
+
+	return;
 
 }
